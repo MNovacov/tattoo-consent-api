ad88d4a046a8e7aa17d5d748b55e11ea0fe1180b
MNovacov <mnovacov@hotmail.com>
2025-06-03 14:49:43 -0400

    Form de menores para notion

--- api/addd.js
+++ api/addd.js
@@ -90,9 +90,11 @@ export default async function handler(req, res) {
     Fecha: { date: { start: Fecha } },
     Valor: Valor ? { number: parseInt(Valor) } : undefined,
     Abono: Abono ? { number: parseInt(Abono) } : undefined,
-    Alergias: { rich_text: [{ text: { content: Alergias } }] },
     "Firma Cliente": { url: FirmaCliente },
+    "Firma Tatuador": { url: FirmaTatuador },
+    "Menor de Edad": { checkbox: true },
   };
+
   const properties = Object.fromEntries(
     Object.entries(rawProperties).filter(([, v]) => v !== undefined)
   );
